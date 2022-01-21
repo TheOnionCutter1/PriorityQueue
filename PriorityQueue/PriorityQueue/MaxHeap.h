509b872f076a1435cd415b5311b35fb7d40ace05
Omer <omeralon53@gmail.com>
2022-01-21 09:54:58 +0200

    Changes in MaxHeap: changed constructor added insert method declaration added getMax method

--- PriorityQueue/PriorityQueue/MaxHeap.h
+++ PriorityQueue/PriorityQueue/MaxHeap.h
@@ -16,7 +16,7 @@ private:
 	// private methods
 	MaxHeap(std::vector<T>* items, size_t index);
 public:
-	MaxHeap();
+	MaxHeap(T value);
 	~MaxHeap();
 	
 	void insert(T value);
@@ -25,17 +25,43 @@ public:
 	T getMax() const;
 };
 
+/* Create a childless heap node */
 template<typename T>
-inline MaxHeap<T>::MaxHeap()
+inline MaxHeap<T>::MaxHeap(std::vector<T>* items, size_t index) :
+	_items(items), _index(index)
+{
+	this->_left = nullptr;
+	this->_right = nullptr;
+}
+
+template<typename T>
+inline MaxHeap<T>::MaxHeap(T value)
 {
 	this->_items = new std::vector<T>();
 	this->_index = 0;
 	this->_left = nullptr;
 	this->_right = nullptr;
+
+	this->_items->push_back(value);
 }
 
 template<typename T>
 inline MaxHeap<T>::~MaxHeap()
 {
 	delete this->_items;
+}
+
+/*
+Insert a value to the heap.
+Input: the value.
+*/
+template<typename T>
+inline void MaxHeap<T>::insert(T value)
+{
+}
+
+template<typename T>
+inline T MaxHeap<T>::getMax() const
+{
+	return (*this->_items)[0];
 }
