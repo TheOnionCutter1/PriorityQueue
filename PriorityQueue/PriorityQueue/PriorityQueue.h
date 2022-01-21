275d50a7035b381d9ebe8acbe794705b478b70d9
Omer <omeralon53@gmail.com>
2022-01-21 14:56:22 +0200

    implemented all of the functions of PriorityQueue

--- PriorityQueue/PriorityQueue/PriorityQueue.h
+++ PriorityQueue/PriorityQueue/PriorityQueue.h
@@ -31,6 +31,52 @@ inline PriorityQueue<T>::~PriorityQueue()
 	delete this->_root;
 }
 
+/*
+Push a value to the queue.
+Input: the value.
+*/
+template<typename T>
+inline void PriorityQueue<T>::push(const T& value)
+{
+	if (this->_root)
+	{
+		this->_root->insert(value);
+	}
+	else
+	{
+		this->_root = new MaxHeap<T>(value);
+	}
+}
+
+/* Pop the first value in the queue. */
+template<typename T>
+inline void PriorityQueue<T>::pop()
+{
+	if (this->_root->size() == 1)
+	{
+		delete this->_root;
+		this->_root = nullptr;
+	}
+	else
+	{
+		this->_root->extractMax();
+	}
+}
+
+/* Get the first value in the queue */
+template<typename T>
+inline const T& PriorityQueue<T>::top() const
+{
+	return this->_root->getValue();
+}
+
+/* Check if the queue is empty */
+template<typename T>
+inline bool PriorityQueue<T>::empty() const
+{
+	return this->_root->size() == 0;
+}
+
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const PriorityQueue<T>& q)
 {
